21305ca8e1a0fb2a8b6740aa3ac6aa9dd8aacbcb
na-minseo <130384901+na-minseo@users.noreply.github.com>
2023-04-16 22:26:45 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+/* 버튼 스타일링 예시 */
+button {
+  width: 100%;
+  height: 40px;
+  font-size: 18px;
+  border: none;
+  border-radius: 5px;
+  background-color: #ffcccb; /* 버튼 배경색 */
+  color: #333333;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #ff9999; /* 버튼 호버 배경색 */
+  color: #ffffff; /* 호버 시 글자색 변경 */
+}
+
+/* 계산기 배경색 및 폰트 설정 예시 */
+#calculator {
+  max-width: 200px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #e0e0e0; /* 계산기 배경색 */
+  border-radius: 10px; /* 계산기 테두리 둥글게 */
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  font-family: 'Noto Sans KR', sans-serif; /* 원하는 폰트로 변경 */
+}
